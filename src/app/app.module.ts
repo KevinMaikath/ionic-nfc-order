dee443e91e06ca91f0ab8bf97459aac229452b7e
kevin <kevinrx25@gmail.com>
2019-10-14 13:20:23 +0100

    Initial commit with Catalog page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,10 +9,21 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+// Firebase modules
+import {AngularFireModule} from '@angular/fire';
+import {AngularFireDatabaseModule} from '@angular/fire/database';
+import {environment} from '../environments/environment';
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+      BrowserModule,
+      IonicModule.forRoot(),
+      AppRoutingModule,
+      AngularFireModule.initializeApp(environment.firebase),
+      AngularFireDatabaseModule
+  ],
   providers: [
     StatusBar,
     SplashScreen,
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+    production: false,
+    firebase: {
+        apiKey: 'AIzaSyDmtkR9b1c79Wo7pWSm04lYTpS2MDTQKjk',
+        authDomain: 'app-pem.firebaseapp.com',
+        databaseURL: 'https://app-pem.firebaseio.com',
+        projectId: 'app-pem',
+        storageBucket: 'app-pem.appspot.com',
+        messagingSenderId: '403715151292',
+        appId: '1:403715151292:web:8f7c3f1ec40499212e07b9'
+    }
 };
 
 /*
