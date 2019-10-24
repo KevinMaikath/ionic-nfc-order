eb8e215cf57f765e8f09116013c288072e5096fa
Kevin <kevinrx25@gmail.com>
2019-10-24 20:58:45 +0100

    Lectura de NFC implementada

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,34 +1,39 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { RouteReuseStrategy } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {RouteReuseStrategy} from '@angular/router';
 
-import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
-import { StatusBar } from '@ionic-native/status-bar/ngx';
+import {IonicModule, IonicRouteStrategy} from '@ionic/angular';
+import {SplashScreen} from '@ionic-native/splash-screen/ngx';
+import {StatusBar} from '@ionic-native/status-bar/ngx';
 
-import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
+import {AppComponent} from './app.component';
+import {AppRoutingModule} from './app-routing.module';
 
 // Firebase modules
 import {AngularFireModule} from '@angular/fire';
 import {AngularFirestoreModule} from '@angular/fire/firestore';
 import {environment} from '../environments/environment';
 
+// NFC module
+import {NFC} from '@ionic-native/nfc/ngx';
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
   imports: [
-      BrowserModule,
-      IonicModule.forRoot(),
-      AppRoutingModule,
-      AngularFireModule.initializeApp(environment.firebase),
-      AngularFirestoreModule
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule,
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+    NFC,
+    {provide: RouteReuseStrategy, useClass: IonicRouteStrategy}
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule {
+}
--- src/app/pages/shopping-cart/shopping-cart.page.ts
+++ src/app/pages/shopping-cart/shopping-cart.page.ts
@@ -1,6 +1,10 @@
 import {Component, OnInit} from '@angular/core';
 import {ShopItem} from '../../models/shop-item';
 import {ShoppingService} from '../../services/shopping.service';
+import {LoadingController, AlertController} from '@ionic/angular';
+import {NFC, NdefEvent} from '@ionic-native/nfc/ngx';
+import {Observable, Subscription} from 'rxjs';
+
 
 @Component({
   selector: 'app-shopping-cart',
@@ -11,8 +15,17 @@ export class ShoppingCartPage implements OnInit {
 
   shoppingCart: ShopItem[];
   totalPrice: number;
+  loading: HTMLIonLoadingElement;
+  listenAlert: HTMLIonAlertElement;
 
-  constructor(private shoppingService: ShoppingService) {
+  existingObservable = false;
+  ndefEventObservable: Observable<NdefEvent>;
+  nfcSubscription: Subscription;
+
+  constructor(private shoppingService: ShoppingService,
+              private alertCtrl: AlertController,
+              private loadingCtrl: LoadingController,
+              private nfc: NFC) {
   }
 
   ngOnInit() {
@@ -33,5 +46,82 @@ export class ShoppingCartPage implements OnInit {
     this.shoppingCart = this.shoppingService.getShoppingCart();
     this.totalPrice = this.shoppingService.getTotalPrice();
   }
+
+  onDoneClicked() {
+    this.setNdefListener();
+    this.setNdefSubscription();
+
+    // this.loading = await this.loadingCtrl.create({
+    //   message: 'Wait a second...',
+    //   duration: 3000
+    // });
+    //
+    // await this.loading.present();
+
+  }
+
+  // Check if the listener is currently on,
+  // so that we dont add multiple listeners
+  private setNdefListener() {
+    if (!this.existingObservable) {
+      this.ndefEventObservable = this.nfc.addNdefListener(() => {
+        this.existingObservable = true;
+      }, (err) => {
+        this.existingObservable = false;
+        this.alertCtrl.create({
+          message: 'Please enable NFC first',
+          buttons: [
+            {
+              text: 'Okay',
+              role: 'cancel'
+            }
+          ]
+        }).then(alertEl => {
+          alertEl.present();
+        });
+      });
+    }
+  }
+
+  private setNdefSubscription() {
+    this.nfcSubscription = this.ndefEventObservable.subscribe((event) => {
+      this.onNdefEvent(event);
+    });
+    this.setReadNfcAlert();
+  }
+
+  private onNdefEvent(event) {
+    this.listenAlert.dismiss();
+
+    // Read from register 4
+    let payload = this.nfc.bytesToString(event.tag.ndefMessage[4].payload);
+    payload = payload.substring(3);
+    this.alertCtrl.create({
+      message: payload,
+      buttons: [
+        {text: 'Cancel'}
+      ]
+    }).then(alertEl => {
+      alertEl.present();
+    });
+
+    this.nfcSubscription.unsubscribe();
+  }
+
+  private async setReadNfcAlert() {
+    this.listenAlert = await this.alertCtrl.create({
+      message: 'Please approach your phone to the NFC tag',
+      buttons: [
+        {
+          text: 'Cancel',
+          handler: () => {
+            this.nfcSubscription.unsubscribe();
+          }
+        }
+      ]
+    });
+    await this.listenAlert.present();
+  }
+
 
 }
