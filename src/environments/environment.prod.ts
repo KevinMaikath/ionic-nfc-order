a7e7402e2f7547265678706744537f4a39ae094b
Kevin <kevinrx25@gmail.com>
2019-11-25 19:09:59 +0000

    Firebase deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyDmtkR9b1c79Wo7pWSm04lYTpS2MDTQKjk',
+    authDomain: 'app-pem.firebaseapp.com',
+    databaseURL: 'https://app-pem.firebaseio.com',
+    projectId: 'app-pem',
+    storageBucket: 'app-pem.appspot.com',
+    messagingSenderId: '403715151292',
+    appId: '1:403715151292:web:8f7c3f1ec40499212e07b9'
+  }
 };
